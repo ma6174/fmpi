7501f5c233a0dc3e3e7a71925effe8834991615d
ma6174 <ma617495@gmail.com>
2019-07-09 22:46:51 +0800

    remove online music

--- fmpi.py
+++ fmpi.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python
 # coding=utf-8
-import os
 import time
-import random
-import douban
 import config
 import logging
 import subprocess
-from db import DB
-from get_sogou_mp3 import getlink, get_all_mp3
 
-
-class FMPI(DB):
-
-    '''从播放队列获取歌曲并播放'''
-
+class FMPI():
     def play(self, name_or_url, freq=98.5, rate=44100):
         '''调用外部播放命令'''
 # cmd = "mpg123 -m -C -q -s %s | sudo pifm - %s
 # %s"%(name_or_url,freq,rate)
         cmd1 = "mpg123 -m -C -q -s %s" % name_or_url
         cmd2 = "sudo pifm - %s %s" % (freq, rate)
-        self.print_local_ip()
-        logging.info(
-            '''press q to play next songs,\npress Ctrl+c to terminate''')
-        logging.info("")
         self.p1 = subprocess.Popen(cmd1, shell=True, stdout=subprocess.PIPE)
         self.p2 = subprocess.Popen(
             cmd2,
@@ -32,56 +19,4 @@ class FMPI(DB):
             stdin=self.p1.stdout,
             stdout=subprocess.PIPE)
         self.p1.wait()
-#        os.system(cmd)
         return 0
-    def print_local_ip(self):
-        cmd = "hostname -I"
-        logging.info("your IP is:")
-        p = subprocess.Popen(cmd,shell=True)
-        p.wait()
-        logging.info("Just open http://Your_IP:8080/ in broswer to add music")
-
-    def control(self, key):
-        '''has problem'''
-        out, err = self.p1.communicate(key)
-        return out
-
-    def get_random_music(self):
-        '''获取随机歌曲'''
-        music_set = file("./music_name.txt").readlines()
-        total = len(music_set)
-        rand = random.randint(0, total - 1)
-        return music_set[rand][:-1]
-
-    def get_douban_music(self):
-        '''获取豆瓣歌曲'''
-        song = douban.getmusic()
-        return song['title']
-
-    def fmpi(self):
-        '''循环检测'''
-        while True:
-            query = DB.getall(self)
-            try:
-                one = query[0]
-            except:
-                one = None
-            os.system("clear")
-            if one is not None:
-                logging.info('>>>>%s' % one[1])
-                if one[1].startswith('http://'):  # 直接播放url对应的音乐
-                    if one[1].endswith('.mp3'):
-                        self.play(one[1], config.freq, config.rate)
-                    else:
-                        links = get_all_mp3(one[1])
-                        for i in links:
-                            DB.put(self, i)
-                else:
-                    url = getlink(one[1].encode('utf-8'))
-                    self.play(url, config.freq, config.rate)
-                DB.updateone(self, one[0])
-            else:
-#                rand_music = self.get_random_music()
-                douban_music = self.get_douban_music()
-                DB.put(self, douban_music)
-            time.sleep(1)  # 降低CPU占用率
--- web_server.py
+++ web_server.py
@@ -2,7 +2,6 @@
 #coding=utf-8
 import web
 import logging
-from db import DB
 from fmpi import FMPI
 from threading import Thread
 urls = (
@@ -10,69 +9,33 @@ urls = (
 )
 app = web.application(urls,globals())
 web.config.debug = False
+pi = FMPI()
 
-class INDEX(DB,FMPI):
+class INDEX(FMPI):
     '''web页面相关'''
     def index(self):
         html = '''<head>
         <meta charset="UTF-8">
         <link rel="shortcut icon" href="/static/favicon.ico" >
-        <meta http-equiv="refresh" content="5">
         </head>
         <html>
         <form action="/" method="GET">
-        I want to listen:
+        music path or url:
         <input type="TEXT" name="m" />
-        <input type="submit" value="add to list"  />
+        <input type="submit" value="play"  />
         </from>
+        </html>
          '''
-        query = DB.getall(self)
-        try:
-            html += u"<h2>Now Playing</h1>%s<h2>list to be played</h1>"%query[0][1]
-            num = 1
-            for i in query[1:]:
-                html = html + '<li>%d----%s</li>'%(num,i[1])
-                num+=1
-            return html
-        except:
-            return html
-    def check_name_exist(self,name):
-        '''检查歌曲名字是否存在'''
-        all = DB.getall(self)
-        for i in all:
-            logging.info(i[1])
-            if i[1] == name:
-                return True
-        return False
-    def add_music(self,music_name):
-        if self.check_name_exist(music_name) is False:
-            DB.put(self,music_name)
-            return None
-        else:
-            return '''<head><meta charset="UTF-8"></head>
-            <h1>music already exists'''
+        return html
 
     def GET(self,args=None):
         input = web.input()
-        if input.has_key('c'):
-            key = input['c']
-#            FMPI.control(self,key)
-            web.seeother('/')
         if input.has_key('m'):
-            music_name = input['m']
-            ret = self.add_music(music_name)
-            if ret is None:
-                web.seeother('/')
-            else:
-                return ret
+            key = input['m']
+            logging.info("play %s"%key)
+            pi.play(key)
         return self.index()
 
 if __name__=='__main__':
     logging.basicConfig(level=logging.INFO,format="%(message)s")
-    db = DB()
-    db.create_table()
-    pi = FMPI()
-    player = Thread(target=pi.fmpi) #播放器线程
-    player.setDaemon(True)     #随主线程一块退出
-    player.start()
     app.run() #web服务器--主线程
